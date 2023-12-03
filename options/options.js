07398975a11427baa0933959037df4bfc206f8bb
Crowdbotics <team@crowdbotics.com>
2023-12-03 06:53:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pandorabox-44849",
+  url: "https://pandorabox-44849.botics.co",
+  api: "https://pandorabox-44849.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
